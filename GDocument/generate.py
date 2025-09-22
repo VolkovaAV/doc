d038adff8d24d26d32eb6d6ef5ffb8914e18c9bb
Aleksandra <grays.rf@gmail.com>
2025-09-22 12:18:13 +0300

    clear code

--- GDocument/generate.py
+++ GDocument/generate.py
@@ -38,19 +38,8 @@ from docx.shared import Mm
 import os
 from typing import Dict, Any, Optional
 
-NameOrg = 'МЦФПИН'
-PersonalAcc = '40703810942000000672'
-BankName= 'ВОЛГО-ВЯТСКИЙ БАНК ПАО СБЕРБАНК'
-BIC = '042202603'
-CorrespAcc='30101810900000000603'
-KPP='526001001'
-PayeeINN='5260054053'
 
-from_mail = "pay.incas@mail.ru"                         # Почта отправителя
-from_passwd = "8sRt0H0xDvqTmCxhNGx8"                      # пароль от почты отправителя
-server_adr = "smtp.mail.ru"                               # адрес почтового сервера
 
-to_mail_test = 'a.volkova@ipfran.ru'                           # адрес получателя
 
 def pdf(filename, pathname='out'):
     '''
